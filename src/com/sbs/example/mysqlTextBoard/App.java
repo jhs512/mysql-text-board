f1381d24ef3c881a36b19b8a22f48e9f49edaffe
jhs512 <jangka512@gmail.com>
2020-11-17 13:29:36 +0900

    컨트롤러 상위 클래스 도입

--- src/com/sbs/example/mysqlTextBoard/App.java
+++ src/com/sbs/example/mysqlTextBoard/App.java
@@ -2,16 +2,12 @@ package com.sbs.example.mysqlTextBoard;
 
 import java.util.Scanner;
 
-import com.sbs.example.mysqlTextBoard.controller.ArticleController;
-import com.sbs.example.mysqlTextBoard.controller.MemberController;
+import com.sbs.example.mysqlTextBoard.controller.Controller;
 import com.sbs.example.mysqlutil.MysqlUtil;
 
 public class App {
 	public void run() {
 		Scanner sc = Container.scanner;
-
-		ArticleController articleController = new ArticleController();
-		MemberController memberController = new MemberController();
 
 		while (true) {
 			System.out.printf("명령어) ");
@@ -21,13 +17,14 @@ public class App {
 
 			boolean needToExit = false;
 
-			if (cmd.startsWith("article ")) {
-				articleController.doCommand(cmd);
-			} else if (cmd.startsWith("member ")) {
-				memberController.doCommand(cmd);
-			} else if (cmd.equals("system exit")) {
+			if (cmd.equals("system exit")) {
 				System.out.println("== 시스템 종료 ==");
 				needToExit = true;
+			} else {
+				Controller controller = getControllerByCmd(cmd);
+				if (controller != null) {
+					controller.doCommand(cmd);
+				}
 			}
 
 			MysqlUtil.closeConnection();
@@ -36,5 +33,15 @@ public class App {
 				break;
 			}
 		}
+	}
+
+	private Controller getControllerByCmd(String cmd) {
+		if (cmd.startsWith("article ")) {
+			return Container.articleController;
+		} else if (cmd.startsWith("member ")) {
+			return Container.memberController;
+		}
+
+		return null;
 	}
 }
--- src/com/sbs/example/mysqlTextBoard/Container.java
+++ src/com/sbs/example/mysqlTextBoard/Container.java
@@ -2,20 +2,31 @@ package com.sbs.example.mysqlTextBoard;
 
 import java.util.Scanner;
 
+import com.sbs.example.mysqlTextBoard.controller.ArticleController;
+import com.sbs.example.mysqlTextBoard.controller.Controller;
+import com.sbs.example.mysqlTextBoard.controller.MemberController;
 import com.sbs.example.mysqlTextBoard.service.ArticleService;
 import com.sbs.example.mysqlTextBoard.service.MemberService;
 
 public class Container {
 
 	public static Scanner scanner;
+	
 	public static MemberService memberService;
 	public static ArticleService articleService;
+	
+	public static Controller articleController;
+	public static Controller memberController;
 
 	static {
 		scanner = new Scanner(System.in);
 
 		memberService = new MemberService();
 		articleService = new ArticleService();
+		
+		articleController = new ArticleController();
+		memberController = new MemberController();
+
 	}
 
 }
