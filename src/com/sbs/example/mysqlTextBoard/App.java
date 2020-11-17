24de8ccb014d76d9e078be7741a18b0391b840a9
jhs512 <jangka512@gmail.com>
2020-11-17 11:10:11 +0900

    ArticleDao::getArticles에 MySQLUtil 적용

--- src/com/sbs/example/mysqlTextBoard/App.java
+++ src/com/sbs/example/mysqlTextBoard/App.java
@@ -3,6 +3,7 @@ package com.sbs.example.mysqlTextBoard;
 import java.util.Scanner;
 
 import com.sbs.example.mysqlTextBoard.controller.ArticleController;
+import com.sbs.example.mysqlutil.MysqlUtil;
 
 public class App {
 	public void run() {
@@ -14,10 +15,20 @@ public class App {
 			System.out.printf("명령어) ");
 			String cmd = sc.nextLine();
 
+			MysqlUtil.setDBInfo("127.0.0.1", "sbsst", "sbs123414", "textBoard");
+
+			boolean needToExit = false;
+
 			if (cmd.startsWith("article ")) {
 				articleController.doCommand(cmd);
 			} else if (cmd.equals("system exit")) {
 				System.out.println("== 시스템 종료 ==");
+				needToExit = true;
+			}
+			
+			MysqlUtil.closeConnection();
+
+			if (needToExit) {
 				break;
 			}
 		}
