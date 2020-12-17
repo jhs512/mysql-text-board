5185eb4e3f60150d9111dd230945d2de25dd7547
jhs512 <jangka512@gmail.com>
2020-12-17 12:01:56 +0900

    빌드할때마다 site 폴더 지우는 로직제거

--- src/com/sbs/example/mysqlTextBoard/service/BuildService.java
+++ src/com/sbs/example/mysqlTextBoard/service/BuildService.java
@@ -19,7 +19,6 @@ public class BuildService {
 
 	public void buildSite() {
 		System.out.println("site/article 폴더 생성");
-		Util.rmdir("site");
 		Util.mkdirs("site");
 
 		Util.copy("site_template/app.css", "site/app.css");
