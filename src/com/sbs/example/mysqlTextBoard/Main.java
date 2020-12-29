1d4d3bd9fb279063ccb5cf22eb6991e831a57c0f
jhs512 <jangka512@gmail.com>
2020-12-29 12:27:12 +0900

    api 로 받아온 json 데이터를 잭슨을 이용하여 다양한 객체로 매핑

--- src/com/sbs/example/mysqlTextBoard/Main.java
+++ src/com/sbs/example/mysqlTextBoard/Main.java
@@ -1,16 +1,10 @@
 package com.sbs.example.mysqlTextBoard;
 
-import java.util.List;
-import java.util.Map;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sbs.example.mysqlTextBoard.test.TestRunner;
-import com.sbs.example.mysqlTextBoard.util.Util;
 
 public class Main {
 	public static void main(String[] args) {
-		new TestRunner().run();
-		// new App().run();
+		// new TestRunner().run();
+		new App().run();
 	}
 }
--- src/com/sbs/example/mysqlTextBoard/test/TestRunner.java
+++ src/com/sbs/example/mysqlTextBoard/test/TestRunner.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sbs.example.mysqlTextBoard.apidto.DisqusApiDataListThread;
 import com.sbs.example.mysqlTextBoard.util.Util;
 
 public class TestRunner {
@@ -18,7 +19,7 @@ public class TestRunner {
 	}
 
 	public void run() {
-		testJackson5();
+		testApi3();
 	}
 
 	private void testJackson() {
@@ -107,5 +108,23 @@ public class TestRunner {
 		String rs = Util.callApi(url, "api_key=GuZRzNgkWLxFPfkg6E0SqICbejq8PimfgTMdFVfWYYedfhmI6kz6g5q1ZmbVAMMW",
 				"forum=my-ssg", "thread:ident=article_detail_2.html");
 		System.out.println(rs);
+	}
+
+	private void testApi2() {
+		String url = "https://disqus.com/api/3.0/forums/listThreads.json";
+		Map<String, Object> rs = Util.callApiResponseToMap(url,
+				"api_key=GuZRzNgkWLxFPfkg6E0SqICbejq8PimfgTMdFVfWYYedfhmI6kz6g5q1ZmbVAMMW", "forum=my-ssg",
+				"thread:ident=article_detail_2.html");
+		List<Map<String, Object>> response = (List<Map<String, Object>>) rs.get("response");
+		Map<String, Object> thread = response.get(0);
+		System.out.println((int) thread.get("likes"));
+	}
+
+	private void testApi3() {
+		String url = "https://disqus.com/api/3.0/forums/listThreads.json";
+		DisqusApiDataListThread rs = (DisqusApiDataListThread) Util.callApiResponseTo(DisqusApiDataListThread.class,
+				url, "api_key=GuZRzNgkWLxFPfkg6E0SqICbejq8PimfgTMdFVfWYYedfhmI6kz6g5q1ZmbVAMMW", "forum=my-ssg",
+				"thread:ident=article_detail_2.html");
+		System.out.println(rs.response.get(0).likes + rs.response.get(0).posts);
 	}
 }
