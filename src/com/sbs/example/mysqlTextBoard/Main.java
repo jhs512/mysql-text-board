8d607e95edda8d2358f8e5c0de3bafaad0069f55
jhs512 <jangka512@gmail.com>
2020-12-29 11:57:01 +0900

    HTTP REQUEST 후 응답 받아오는 함수 구현

--- src/com/sbs/example/mysqlTextBoard/Main.java
+++ src/com/sbs/example/mysqlTextBoard/Main.java
@@ -1,7 +1,18 @@
 package com.sbs.example.mysqlTextBoard;
 
+import com.sbs.example.mysqlTextBoard.util.Util;
+
 public class Main {
 	public static void main(String[] args) {
-		new App().run();
+		testApi();
+
+		// new App().run();
+	}
+
+	private static void testApi() {
+		String url = "https://disqus.com/api/3.0/forums/listThreads.json";
+		String rs = Util.callApi(url, "api_key=GuZRzNgkWLxFPfkg6E0SqICbejq8PimfgTMdFVfWYYedfhmI6kz6g5q1ZmbVAMMW",
+				"forum=my-ssg", "thread:ident=article_detail_2.html");
+		System.out.println(rs);
 	}
 }
